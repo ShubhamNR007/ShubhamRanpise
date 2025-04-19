19e34249dfae764f9601b2426d6ac9b5a9bc3988
shubham <shubhamrnps@gmail.com>
2025-04-19 23:53:44 +0530

    restored to breadcrum, removed thing from nav

--- static/js/main.js
+++ static/js/main.js
@@ -76,21 +76,3 @@ document.querySelectorAll(".download-btn").forEach((btn, index) => {
   });
 });
 
-let lastScrollTop = 0;
-
-window.addEventListener("scroll", () => {
-  const desktopNav = document.getElementById("desktop-nav");
-  const mobileNav = document.getElementById("hamburger-nav");
-  let scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-  const hide = scrollTop > lastScrollTop;
-  const show = scrollTop < lastScrollTop;
-
-  if (window.innerWidth > 768 && desktopNav) {
-    desktopNav.style.top = hide ? "-100px" : "0";
-  } else if (mobileNav) {
-    mobileNav.style.top = hide ? "-100px" : "0";
-  }
-
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-});
